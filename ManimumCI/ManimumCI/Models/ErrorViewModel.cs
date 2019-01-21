18132aeddcae63dbe756568b2b58335f87331bc5
桂素伟 <axzxs2001@163.com>
2019-01-21 18:34:03 +0900

    添加CI项目。

---
+++ ManimumCI/ManimumCI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ManimumCI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
